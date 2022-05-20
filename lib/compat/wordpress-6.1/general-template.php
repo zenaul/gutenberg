ade395b0e7503fb5922d66458e43237615c30c76
Ari Stathopoulos <aristath@gmail.com>
2022-05-20 13:27:06 +0300

    Disable comment feed links for block themes

---
+++ lib/compat/wordpress-6.1/general-template.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Disable comments feed link for block themes.
+ *
+ * @param bool $show Whether to display the comments feed link.
+ * @return bool
+ */
+function wp_disable_comments_feed_for_block_themes( $show ) {
+	return wp_is_block_theme() ? false : $show;
+}
+add_filter( 'feed_links_show_comments_feed', 'wp_disable_comments_feed_for_block_themes' );
