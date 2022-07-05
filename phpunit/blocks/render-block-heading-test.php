3098f5117e3c3fa37122b76534bff19716b48138
Adam Zieliński <adam@adamziel.com>
2022-07-05 17:41:02 +0200

    Add labels to unit tests

--- phpunit/blocks/render-block-heading-test.php
+++ phpunit/blocks/render-block-heading-test.php
@@ -24,51 +24,51 @@ class Render_Block_Heading_Test extends WP_UnitTestCase {
 
 	public function add_css_class_test_examples(  ) {
 		return array(
-			array(
+			'should add a class name to a vanilla h2 element' => array(
 				'<h2>Hello World</h2>',
 				'<h2 class="wp-block-heading">Hello World</h2>'
 			),
-			array(
+			'should not add a class name to a header element' => array(
 				'<header>Hello World</header>',
 				'<header>Hello World</header>',
 			),
-			array(
+			'should not add a class name to a span element' => array(
 				'<span>Hello World</span>',
 				'<span>Hello World</span>'
 			),
-			array(
+			'should not add a class name to an invalid h7 element' => array(
 				'<h7>Hello World</h7>',
 				'<h7>Hello World</h7>'
 			),
-			array(
+			'should add a class name even when the class attribute is already defined' => array(
 				'<h2 class="is-align-right">Hello World</h2>',
 				'<h2 class="is-align-right wp-block-heading">Hello World</h2>'
 			),
-			array(
+			'should not add a class name even when it is already defined' => array(
 				'<h2 class="is-align-right wp-block-heading">Hello World</h2>',
 				'<h2 class="is-align-right wp-block-heading">Hello World</h2>'
 			),
-			array(
+			'should add a class name even when there are other HTML attributes present' => array(
 				'<h2 style="display: block">Hello World</h2>',
 				'<h2 style="display: block" class="wp-block-heading">Hello World</h2>'
 			),
-			array(
+			'should add a class name even when the class attribute is already defined and has many entries' => array(
 				'<h2 class="is-align-right custom   classes">Hello World</h2>',
 				'<h2 class="is-align-right custom classes wp-block-heading">Hello World</h2>'
 			),
-			array(
+			'should not add a class name to a nested h2' => array(
 				'<h2 class="is-align-right custom classes"><h2>Hello World</h2></h2>',
 				'<h2 class="is-align-right custom classes wp-block-heading"><h2>Hello World</h2></h2>'
 			),
-			array(
+			'should not add a class name to a nested h2 when the parent has another attribute' => array(
 				'<h2 style="display: block" class="is-align-right"><h2>Hello World</h2></h2>',
 				'<h2 style="display: block" class="is-align-right wp-block-heading"><h2>Hello World</h2></h2>'
 			),
-			array(
+			'should add a class name even when the class attribute is surrounded by other attributes' => array(
 				'<h2 style="display: block" class="is-align-right" data-class="corner case!"><h2>Hello World</h2></h2>',
 				'<h2 style="display: block" class="is-align-right wp-block-heading" data-class="corner case!"><h2>Hello World</h2></h2>'
 			),
-			array(
+			'should add a class name without getting confused when there is a tricky data-class attribute present' => array(
 				'<h2 data-class="corner case!" style="display: block" class="is-align-right"><h2>Hello World</h2></h2>',
 				'<h2 data-class="corner case!" style="display: block" class="is-align-right wp-block-heading"><h2>Hello World</h2></h2>'
 			)
