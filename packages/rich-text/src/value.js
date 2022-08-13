2b353ebcbd1c54f9431a20ae29449a1fd3d50c9e
Ella van Durpe <ella@vandurpe.com>
2022-08-13 14:09:02 +0200

    Lazy call toHTMLString

--- packages/rich-text/src/value.js
+++ packages/rich-text/src/value.js
@@ -3,15 +3,40 @@
  */
 import { toHTMLString } from './to-html-string';
 
-export class RichTextValue extends String {
-	constructor( { value } ) {
-		super( toHTMLString( ...arguments ) );
-
-		for ( const key in value ) {
+export class RichTextValue {
+	constructor( args ) {
+		for ( const key in args.value ) {
 			Object.defineProperty( this, key, {
-				value: value[ key ],
+				value: args.value[ key ],
 				enumerable: true,
 			} );
 		}
+
+		function _toHTMLString( _args ) {
+			if ( ! _toHTMLString.cached ) {
+				_toHTMLString.cached = toHTMLString( _args );
+			}
+			return _toHTMLString.cached;
+		}
+
+		Object.getOwnPropertyNames( String.prototype )
+			.filter(
+				( prop ) =>
+					typeof String.prototype[ prop ] === 'function' &&
+					prop !== 'constructor'
+			)
+			.forEach( ( method ) => {
+				Object.defineProperty( this, method, {
+					value() {
+						return _toHTMLString( args )[ method ]( ...arguments );
+					},
+				} );
+			} );
+
+		Object.defineProperty( this, 'length', {
+			get() {
+				return _toHTMLString( args ).length;
+			},
+		} );
 	}
 }
